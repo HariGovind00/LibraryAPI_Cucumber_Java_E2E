f6a510b944d070fc3618d61b3d5f190339ca4e7b
HariGovind00 <141699967+HariGovind00@users.noreply.github.com>
2025-12-24 16:56:10 +0530

    Update PostAPITestRunner.java

--- src/test/java/CucumberTestRunner/PostAPITestRunner.java
+++ src/test/java/CucumberTestRunner/PostAPITestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/CucumberFeatureFile/PostAPI.feature",
 plugin = "json:target/jsonReports/json-cucumber-report.json",
-glue = "CucumberStepDefinationFile"
+glue = "CucumberStepDefinationFile",
 monochrome = true)
 public class PostAPITestRunner extends AbstractTestNGCucumberTests
 {
